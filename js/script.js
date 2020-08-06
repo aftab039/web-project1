7f9fca65fd83ac76e76c9ea0ec554035fcf615b7
Aftab Husain <aftabhusain687@gmail.com>
2020-08-06 09:42:44 +0530

    add js code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function myfunction(){
+	document.getElementById("demo").innerHTML = "aftab";
+	console.log()
+}
+var itnro ={
+	my intro:{name:"aftab",
+	college:{name:"ACEIT",
+	year:"fourth",
+	specialization:{branch:"ECE"}
+	}
+	},
+	age: 22,
+	city:"sultanpur",
+	state:"up"
+}
