952438202bf00785094a825c7a39d8bf2a094f59
Aftab Husain <aftabhusain687@gmail.com>
2020-08-09 01:08:15 +0530

    add codes

--- js/script.js
+++ js/script.js
@@ -1,45 +1,90 @@
-function myFunction(){
-	document.write(temp)
-	document.write("-- my old array--", city)
+
+// you age 
+function ageindays(){
+var birthyear = prompt("what year were you born .. good friend");
+var ageindays = (2019-birhtyear)*365;
+console.log(ageindays);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // function myFunction(){
+// // 	document.write(temp)
+// // 	document.write("-- my old array--", city)
 	
-		document.getElementById("demo").innerHTML= "sultanpur";
-     console.log (person.state.state2.state3)
-}
-var person ={
-	intro:{state:"rajasthan",
-	city:{city1:"jaipur",place:{
-	hostel:"kukas",college:{
-		name:"aceit"},
-		},
-	},
-},
-	age:22,
-	mob:9929064721,
-	city:"sultanpur"
-}
-var college ={
-	myintro:{state:"up",
-	hostel:{name:"aftab",
-	 branch:"ece"
-      },
+// // 		document.getElementById("demo").innerHTML= "sultanpur";
+// //      console.log (person.state.state2.state3)
+// // }
+// var person ={
+// 	intro:{state:"rajasthan",
+// 	city:{city1:"jaipur",place:{
+// 	hostel:"kukas",college:{
+// 		name:"aceit"},
+// 		},
+// 	},
+// },
+// 	age:22,
+// 	mob:9929064721,
+// 	city:"sultanpur"
+// }
+// var college ={
+// 	myintro:{state:"up",
+// 	hostel:{name:"aftab",
+// 	 branch:"ece"
+//       },
 
-   },
+//    },
 
-}
-var city=[
-	"kanpur", "delhi","sln", "mumbai","jaipur"
-]
-person ={
-	college:"aciet",
-	state:{state1:"rajasthan",
- state2:{state3:"up"},},
-	city:"jaipur",
-	hostel:"fourth"
-}
-//city.push("add1");
-//city.pop();
-//city.shift();
-//city.unshift("");
+// }
+// var city=[
+// 	"kanpur", "delhi","sln", "mumbai","jaipur"
+// ]
+// person ={
+// 	college:"aciet",
+// 	state:{state1:"rajasthan",
+//  state2:{state3:"up"},},
+// 	city:"jaipur",
+// 	hostel:"fourth"
+// }
+// //city.push("add1");
+// //city.pop();
+// //city.shift();
+// //city.unshift("");
 
-//var temp= city.slice(0,3);
-var temp= city.splice(0,3);
+// //var temp= city.slice(0,3);
+// var temp= city.splice(0,3);
