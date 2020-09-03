df384e989940d408b32b8ee534595dccca19e1c4
Aftab Husain <aftabhusain687@gmail.com>
2020-09-03 19:09:09 +0530

    add codes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 // age in days
-function ageindays(){
-var birthyear= prompt("what year you were ...good friend");
-var ageindays= (2019- birthyear)* 365;
+//function ageindays(){
+//var birthyear= prompt("what year you were ...good friend");
+//var ageindays= (2019- birthyear)* 365;
 
-var h1 = document.createElement("h1");
-var textAnswer = document.createTextNode("you are " + ageindays +  "days old");
-h1.setAttribute("id", "ageindays");
-h1.appendChild(textAnswer);
-document.getElementById("flexbox-result") .appendChild(h1);
+//var h1 = document.createElement("h1");
+//var textAnswer = document.createTextNode("you are " + ageindays +  "days old");
+//h1.setAttribute("id", "ageindays");
+//h1.appendChild(textAnswer);
+//document.getElementById("flexbox-result") .appendChild(h1);
 //console.log(ageindays);
 }
-function reset(){
-    document.getElementById("ageindays").remove();
-} 
+//function reset(){
+   // document.getElementById("ageindays").remove();
+//} 
+var School ="sultanpur"
+
+function College(){
+	this.School="Lucknow"
+	console.log(this.School);
+	
+	
+}
+College();
+
+	
