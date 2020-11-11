07104cee7fe52cb3472f599d9a22c606444f0d64
Aftab Husain <aftabhusain687@gmail.com>
2020-11-11 07:50:02 +0530

    add files

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-// age in days
-//function ageindays(){
-//var birthyear= prompt("what year you were ...good friend");
-//var ageindays= (2019- birthyear)* 365;
+// var Fruitarray = ["kiwi","mango","apple","guava","grapes","banana",]
 
-//var h1 = document.createElement("h1");
-//var textAnswer = document.createTextNode("you are " + ageindays +  "days old");
-//h1.setAttribute("id", "ageindays");
-//h1.appendChild(textAnswer);
-//document.getElementById("flexbox-result") .appendChild(h1);
-//console.log(ageindays);
+// function Myfunction(){
+// 	Fruitarray.splice(2,5);
+// 	console.log("show array",Fruitarray);
+// }
+function Myfunction() {
+	let fruits = ["Banana","Orange","Lemon","Apple","Mango"];
+	fruits.splice(1, 3);
+	console.log("show array",fruits);
 }
-//function reset(){
-   // document.getElementById("ageindays").remove();
-//} 
-var School ="sultanpur"
+// var Fruitarray2 = ["f","g","h","i","j","k",]
 
-function College(){
-	this.School="Lucknow"
-	console.log(this.School);
-	
-	
-}
-College();
+// function Myfunction2(){
+// 	Fruitarray2.shift(0,1);
+// 	console.log("show array",Fruitarray2);
+// }
+// var Fruitarray3 = ["1","2","3","4","5","6",]
 
-	
+// function Myfunction3(){
+// 	Fruitarray3.unshift(0,1);
+// 	console.log("show array",Fruitarray3);
+// }
+// var Fruitarray4 = ["4","5","6","7","8","9",]
+
+// function Myfunction4(){
+// 	Fruitarray4.push(0,1);
+// 	console.log("show array",Fruitarray4);
+// }
+
